144c622a968dece75a318e45b9c9a6a0e2e92039
ladegunakarrao <154960333+ladegunakarrao@users.noreply.github.com>
2023-12-29 12:44:55 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,17 +1,1 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-# Taking input from the user
-num = int(input("Enter a number to find its factorial: "))
-
-# Checking if the number is negative
-if num < 0:
-    print("Factorial cannot be found for negative numbers.")
-elif num == 0:
-    print("The factorial of 0 is 1.")
-else:
-    result = factorial(num)
-    print(f"The factorial of {num} is {result}.")
+print("hello")
